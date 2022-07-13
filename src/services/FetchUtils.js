ae65a5cfa6b6c15e0d47fd594c7cde895bac1e98
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-13 17:24:28 -0600

    auth fetch

--- src/services/FetchUtils.js
+++ src/services/FetchUtils.js
@@ -1,8 +1,27 @@
-export async function fetchShows() {
+import { client } from './client';
 
+export async function fetchShows() {
   const rawData = await fetch(`/.netlify/functions/spongebob`);
-  
   const data = await rawData.json();
-  
   return data;
+}
+
+export async function signUpUser(email, password) {
+  const { user } = await client.auth.signUp({ email, password });
+  await createProfile(email);
+  return user;
+}
+
+export async function signInUser(email, password) {
+  const { user } = await client.auth.signIn({ email, password });
+  return user;
+}
+
+export async function createProfile(email) {
+  const { body } = await client.from('user_profiles').insert({ email });
+  return body;
+}
+
+export function getUser() {
+  return client.auth.user();
 }
