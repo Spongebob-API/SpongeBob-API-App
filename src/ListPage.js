d7c6a0c84dd3e93f0d1fdd6719e43203ac70ac34
austinbhan <austinbhan@outlook.com>
2022-07-13 15:11:09 -0700

    Need to render listPage

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { fetchShows } from './services/FetchUtils';
 
-export default function ListPage() {
+export default function ListPage({ episodes }) {
   async function fetch() {
     const data = await fetchShows();
     console.log(data);
@@ -11,6 +11,22 @@ export default function ListPage() {
   }, []);
 
   return (
-    <div>ListPage</div>
+    <div className='shows'>
+      {
+        episodes && episodes.length && episodes.map((episode, i) => {
+          
+          return <div className='single-episode' key={episode.name + i}>
+            {
+              <div>
+                <img src={episode.still_path} height="100"/>
+                <h2>{episode.name}</h2>
+                <p>{episode.episode_number}</p>
+                <p>{episode.overview}</p>
+              </div>
+            }
+          </div>;
+        })
+      }
+    </div>
   );
 }
