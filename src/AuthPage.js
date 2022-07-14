a7b2955955713059e8d13bbe194c4514bf103817
‘AMAYA <aamaya547@gmail.com>
2022-07-14 09:40:20 -0700

    added icons and removed spongebob text

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import { signInUser, signUpUser } from './services/FetchUtils';
-import './Amanda.css';
 import backgroundImg from './background2.jpg';
 
-
+import './Amanda.css';
 
 export default function AuthPage() {
   const [signInEmail, setSignInEmail] = useState('');
   const [signInPass, setSignInPass] = useState('');
   const [signUpEmail, setSignUpEmail] = useState('');
   const [signUpPass, setSignUpPass] = useState('');
-  const [curretUser, setCurrentUser] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
 
   function clearForms() {
     setSignInEmail('');
@@ -24,7 +23,7 @@ export default function AuthPage() {
     const user = await signUpUser(signUpEmail, signUpPass);
     setCurrentUser(user);
     clearForms();
-    return curretUser;
+    return currentUser;
   }
 
   async function handleSignIn(e) {
@@ -32,37 +31,42 @@ export default function AuthPage() {
     const user = await signInUser(signInEmail, signInPass);
     setCurrentUser(user);
     clearForms();
-    return curretUser;
+    return currentUser;
   }
 
-
-
-
   return (
-    <div className='AuthPage' style={{ backgroundImage: `url(${backgroundImg})` }}>
-      <h1 className='header'>SpongeBob APi Search</h1>
-      <img className='SpongeBob' src={'./images/captain.jpg'} height='200'/>
-      <form onSubmit={handleSignUp} className='signUp'>
+    <div className="AuthPage" style={{ backgroundImage: `url(${backgroundImg})` }}>
+      <h1 className="header">SpongeBob APi Search</h1>
+      <img className="SpongeBob" src={'./images/captain.jpg'} height="200" />
+      <form onSubmit={handleSignUp} className="signUp">
         <label>
-                E-mail
-          <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)}/>
+          E-mail
+          <input value={signUpEmail} onChange={(e) => setSignUpEmail(e.target.value)} />
         </label>
         <label>
-            password
-          <input value={signUpPass} type='password' onChange={e => setSignUpPass(e.target.value)}/>
+          password
+          <input
+            value={signUpPass}
+            type="password"
+            onChange={(e) => setSignUpPass(e.target.value)}
+          />
         </label>
-        <button className='button'>SIGN UP!</button>
+        <button className="button">SIGN UP!</button>
       </form>
-      <form onSubmit={handleSignIn} className='signIn'>
+      <form onSubmit={handleSignIn} className="signIn">
         <label>
-                E-mail
-          <input value={signInEmail} onChange={e => setSignInEmail(e.target.value)}/>
+          E-mail
+          <input value={signInEmail} onChange={(e) => setSignInEmail(e.target.value)} />
         </label>
         <label>
-            password
-          <input value={signInPass} type='password' onChange={e => setSignInPass(e.target.value)}/>
+          password
+          <input
+            value={signInPass}
+            type="password"
+            onChange={(e) => setSignInPass(e.target.value)}
+          />
         </label>
-        <button className='button'>SIGN IN!</button>
+        <button className="button">SIGN IN!</button>
       </form>
     </div>
   );
